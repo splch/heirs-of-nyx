7e04ead9445987a5c3ebdbcc4ec991f18f256cd8
Spencer Churchill <spence@duck.com>
2022-06-25 20:15:37 -0700

    make save features expandable

--- src/save.c
+++ src/save.c
@@ -2,30 +2,32 @@
 #include "map.h"
 
 // check if save data exists
-static bool *has_save = (bool *)0xA000;    // pointer to memory address
-static uint16_t *vals = (int16_t *)0xA001; // 16 bit for 2^16 x 2^16 map
+static bool *has_save = (bool *)0xA000;     // pointer to memory address
+static uint16_t *vals = (uint16_t *)0xA001; // 16 bit for 2^16 x 2^16 map
 
 void load_save_data()
 {
-  ENABLE_RAM_MBC5; // for r/w ram address
+  ENABLE_RAM; // for r/w ram address
 
   if (has_save[0] == true) // needs true since any non-zero satisfies the condition
   {
+    uint8_t i = 0;
+
     // starting position for map generation
-    p.x[0] = p.x[1] = vals[0];
-    p.y[0] = p.y[1] = vals[1];
+    p.x[0] = p.x[1] = vals[i++];
+    p.y[0] = p.y[1] = vals[i++];
 
     // player item initialization
-    p.steps = vals[2];
-    p.hearts = vals[3];
-    p.weapons[0] = (int8_t)vals[4];
-    p.weapons[1] = (int8_t)vals[5];
-    p.gold = vals[6];
-    p.maps = vals[7];
+    p.steps = vals[i++];
+    p.hearts = vals[i++];
+    p.weapons[0] = (int8_t)vals[i++];
+    p.weapons[1] = (int8_t)vals[i++];
+    p.gold = vals[i++];
+    p.maps = vals[i++];
 
     // item history data
-    for (uint8_t i = 0; i < 255; i++)
-      arr[i] = vals[i + 8];
+    for (uint8_t j = 0; j < 255 - i; j++) // compiler reduces 255-8->247
+      arr[j] = vals[j + i];               // don't overwrite other save data
   }
   else
   {
@@ -45,23 +47,25 @@ void load_save_data()
 
 void save_data()
 {
-  ENABLE_RAM_MBC5; // TODO: find out what's disabling RAM
+  ENABLE_RAM; // TODO: find out what's disabling RAM
+
+  uint8_t i = 0;
 
   // save position
-  vals[0] = p.x[0];
-  vals[1] = p.y[0];
+  vals[i++] = p.x[0];
+  vals[i++] = p.y[0];
 
   // save items
-  vals[2] = p.steps;
-  vals[3] = p.hearts;
-  vals[4] = p.weapons[0];
-  vals[5] = p.weapons[1];
-  vals[6] = p.gold;
-  vals[7] = p.maps;
+  vals[i++] = p.steps;
+  vals[i++] = p.hearts;
+  vals[i++] = p.weapons[0];
+  vals[i++] = p.weapons[1];
+  vals[i++] = p.gold;
+  vals[i++] = p.maps;
 
   // save item history
-  for (uint8_t i = 8; i < 255; i++)
-    vals[i] = arr[i - 8];
+  for (uint8_t j = i; j < 255; j++)
+    vals[j] = arr[j - i];
 
   // save is now true
   has_save[0] = true;
